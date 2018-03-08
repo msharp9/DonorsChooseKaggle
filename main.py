a9be7354a8b8ba7f05196ae90439bb40432d20ef
msharp9 <msharp9@outlook.com>
2018-03-07 21:07:27 -0700

    Added notes from previous project on text sentiment analysis as well as basic outline of main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# imports
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+
+# pull in data
+
+# clean data (remove punctuation and captilization)
+
+# tokenize data (find stem, remove stopwords)
+
+# Vectorize, Term frequency-inverse document and Normalize
+
+# Train on bag of words
