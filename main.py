d809282f5ff6a3d0f53528db3aed2df02bc9fc95
msharp9 <msharp9@outlook.com>
2018-03-19 20:53:34 -0600

    Added date filter and label encoder

--- main.py
+++ main.py
@@ -2,16 +2,44 @@
 import re
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+# from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
 import nltk
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
+from tqdm import tqdm
+import datetime
+
+# They changed the essay questions on this date
+FILTER_DATE = datetime.datetime(2017,5,17,10,0,0,0)
 
 # pull in data
 df = pd.read_csv('train.csv')
+
+# Filter data due to question change
+print(df.shape)
+df = df[df['project_essay_3'].isnull() & df['project_essay_4'].isnull()]
+print(df.shape)
+
+# Convert Dates to Int
+df['project_submitted_datetime'] = pd.to_datetime(df['project_submitted_datetime']).values.astype(np.int64)
+print('Dates:', df['project_submitted_datetime'].head(3))
+
+# Convert categortical data w/ label encoder
+print('Label Encoder...')
+cat_cols = ['teacher_id', 'teacher_prefix', 'school_state', 'project_grade_category', 'project_subject_categories', 'project_subject_subcategories']
+for c in tqdm(cat_cols):
+    le = LabelEncoder()
+    df[c] = le.fit_transform(df[c].astype(str))
+    # test[c] = le.transform(test[c].astype(str))
+print('Label Encoded Cols:', df[cat_cols].head(3))
+
+
 text_columns = ['project_title', 'project_essay_1', 'project_essay_2', 'project_essay_3', 'project_essay_4', 'project_resource_summary']
 print('Excerpt of the dataset', df[text_columns].head(3))
+
+
 
 # clean data (remove punctuation and captilization)
 def preprocessor(text):
@@ -26,16 +54,16 @@ def tokenizer_porter(text):
     stems = [porter.stem(word) for word in text.split() if word not in stop]
     return ' '.join(stems)
 
-# applying
-for column in text_columns:
-    df[column].fillna('', inplace=True)
-    df[column] = df[column].apply(preprocessor)
-    df[column] = df[column].apply(tokenizer_porter)
-
-print(df[text_columns].head(10))
-
-# Save preprocessing
-df.to_csv('train_pre.csv')
+# # applying
+# for column in tqdm(text_columns):
+#     df[column].fillna('', inplace=True)
+#     df[column] = df[column].apply(preprocessor)
+#     df[column] = df[column].apply(tokenizer_porter)
+#
+# print(df[text_columns].head(10))
+#
+# # Save preprocessing
+# df.to_csv('train_pre.csv')
 
 
 # Vectorize, Term frequency-inverse document and Normalize
--- test.py
+++ test.py
@@ -12,3 +12,15 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 tfidf = TfidfTransformer(use_idf=True, norm='l2', smooth_idf=True)
 print(tfidf.fit_transform(bag).toarray())
+
+
+import datetime
+
+print(datetime.datetime(2017,5,17,10,0,0,0))
+
+
+import time
+from datetime import datetime
+timestamp = int(time.mktime(datetime.now().timetuple()))
+now = datetime.fromtimestamp(timestamp)
+print(timestamp,now)
